b2d9423e0900db92d057a1cc60fcef5622e8d960
Sapir Ben Haim <111499101+sapirbenhaim@users.noreply.github.com>
2023-09-17 21:50:28 +0300

    Added mobile menu
    Added mobile menu to detections page

--- frontend/src/pages/Detections/Detections.jsx
+++ frontend/src/pages/Detections/Detections.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SideMenu from '../../components/SideMenu/SideMenu';
-
 import './Detections.css';
 import Detection from '../../components/Detection/Detection';
+import HamburgerMenu from '../../img/menu-hamburger.png';
+import MobileMenu from '../../components/MobileMenu/MobileMenu';
 
 /**
  * The Detections component represents the page for displaying the latest detections.
  * @returns {JSX.Element} The Detections page content.
  */
 const Detections = () => {
+
+  const [mobileMenu, setMobileMenu] = useState(false);
+
   return (
     <div id='detections'>
       {/* Render the SideMenu component */}
       <SideMenu />
+      {mobileMenu && <MobileMenu setMobileMenu={setMobileMenu} />}
+      {/* Render the mobile menu button */}
+      <img
+        onClick={() => setMobileMenu(!mobileMenu)}
+        className='menuButton'
+        src={HamburgerMenu}
+        alt="Mobile menu button"
+      />
       {/* Page title */}
       <h3 className='pageTitle'>Latest Detections</h3>
       <div className='detectionsGrid'>
